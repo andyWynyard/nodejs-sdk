aac19acd1aff0827e5937dd4bae96e3d7f478599
andyWynyard <andrew.wynyard@gmail.com>
2020-09-04 01:33:17 +0200

    new theme

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,22 +1,11 @@
-module.exports = {
 
-  apiEndpoint: 'https://your-repo-name.prismic.io/api/v2',
+      module.exports = {
 
-  // -- Access token if the Master is not open
-  // accessToken: 'xxxxxx',
-
-  // OAuth
-  // clientId: 'xxxxxx',
-  // clientSecret: 'xxxxxx',
-
-  // -- Links resolution rules
-  // This function will be used to generate links to Prismic.io documents
-  // As your project grows, you should update this function according to your routes
-  linkResolver(doc) {
-    if (doc.type === 'page') {
-      return `/page/${doc.uid}`;
-    }
-
-    return '/';
-  },
-};
+        apiEndpoint: 'https://test-cli.cdn.prismic.io/api/v2',
+      
+        linkResolver: function(doc, ctx) {
+          if (doc.type == 'page') return '/page/' + doc.uid;
+          return '/';
+        }
+      };
+    
